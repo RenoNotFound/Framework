0cd64cb2794a57f60f4057d8b36ffbdfb1caacb1
reClicked <gamper.renato@gmail.com>
2021-01-12 21:00:42 +0100

    Create server class to help with managing superglobal operations

--- Framework/SuperGlobal/Server.php
+++ Framework/SuperGlobal/Server.php
@@ -6,5 +6,18 @@ namespace Framework\SuperGlobal;
 
 class Server
 {
+    public static function getPath()
+    {
+        $path = $_SERVER["REQUEST_URI"] ?? "/";
+        $position = strpos($path, "?");
+        if ($position === false) {
+            return $path;
+        }
+        return substr($path, 0, $position);
+    }
 
+    public static function getMethod()
+    {
+        return strtolower($_SERVER["REQUEST_METHOD"]);
+    }
 }
