e7f725e7bd5ee51b4811c9482b21859b120be2c8
reClicked <gamper.renato@gmail.com>
2021-01-12 21:50:47 +0100

    Create custom template engine

---
+++ Framework/View/View.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Framework\View;
+
+
+class View
+{
+    private string $directory;
+
+    /**
+     * Templeton constructor.
+     * @param string $directory
+     */
+    public function __construct(string $directory)
+    {
+        $this->directory = $directory;
+    }
+
+    public function run(string $templateName, array $templateVariables) : string {
+        $file = $this->directory . $templateName;
+        if (!file_exists($file)) {
+            return "Error loading template file ($file).";
+        }
+        $output = file_get_contents($file);
+
+        foreach ($templateVariables as $key => $value) {
+            $tagToReplace = "{{ $key }}";
+            $output = str_replace($tagToReplace, $value, $output);
+        }
+
+        return $output;
+    }
+}
