1b74a6b287102faaf4e75c81802a61d7baca9626
reClicked <gamper.renato@gmail.com>
2021-01-12 21:50:26 +0100

    Create router class to get and resolve paths

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -4,7 +4,27 @@
 namespace Framework\Router;
 
 
+use Framework\SuperGlobal\Server;
+
 class Router
 {
+    protected array $routes = [];
 
+    public function get($path, $callback)
+    {
+        $this->routes["get"][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = Server::getPath();
+        $method = Server::getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false) {
+            return "Not found";
+        }
+
+        return call_user_func($callback);
+    }
 }
