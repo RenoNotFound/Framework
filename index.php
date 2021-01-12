9a4e9b079b54948308496ccf93468c628557f106
reClicked <gamper.renato@gmail.com>
2021-01-12 21:53:29 +0100

    Initiate application

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "hello";
+use App\Application;
+
+require_once("vendor/autoload.php");
+
+
+$app = new Application(__DIR__);
+
+
+$app->getRouter()->get("/", function () {
+    return "Hello world";
+});
+
+$app->getRouter()->get("/register", function () {
+    return "Register";
+});
+
+$app->run();
