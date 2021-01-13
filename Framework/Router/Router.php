ad2f594e4c1543d5a3ec16ed95fe9275a90442d6
reClicked <gamper.renato@gmail.com>
2021-01-13 02:22:43 +0100

    Implement template rendering

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -5,14 +5,31 @@ namespace Framework\Router;
 
 
 use Framework\SuperGlobal\Server;
+use Framework\View\View;
 
 class Router
 {
+    private View $view;
     protected array $routes = [];
+
+    /**
+     * Router constructor.
+     * @param $rootPath
+     */
+    public function __construct($rootPath)
+    {
+        $this->view = new View($rootPath);
+    }
+
 
     public function get($path, $callback)
     {
         $this->routes["get"][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes["post"][$path] = $callback;
     }
 
     public function resolve()
@@ -22,9 +39,30 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
-            return "Not found";
+            return "Page not found";
+        }
+
+        if (is_string($callback)) {
+            return $this->view($callback);
         }
 
         return call_user_func($callback);
     }
+
+    /**
+     * @return View
+     */
+    public function getView() : View
+    {
+        return $this->view;
+    }
+
+    public function view(string $template, array $variables = []) : string
+    {
+        return $this->view->render($template, $variables);
+    }
+
+
+
+
 }
