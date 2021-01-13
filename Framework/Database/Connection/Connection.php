b4815b7cbdf410b689a53ab05ce7244ef3778806
reClicked <gamper.renato@gmail.com>
2021-01-13 02:21:35 +0100

    Rollback class to initial phase

--- Framework/Database/Connection/Connection.php
+++ Framework/Database/Connection/Connection.php
@@ -9,26 +9,97 @@ use PDOException;
 
 class Connection
 {
+    private static ?Connection $instance = null;
+    private PDO $pdo;
 
-    public static function getConnection(array $config) : PDO
+    private string $host;
+    private string $name;
+    private string $user;
+    private string $password;
+    private array $drivers;
+
+    private function __construct()
+    {
+        $this->connect();
+    }
+
+    public static function configureDatabase(array $config) : void
     {
         $host = $config["database"]["host"];
         $name = $config["database"]["name"];
         $user = $config["database"]["user"];
-        $password = $config["database"]["password"];
+        $host = $config["database"]["password"];
         $drivers = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
         ];
+    }
 
-        $pdo = null;
-        $dsn = "mysql:dbname={$name};host={$host};charset=utf8";
+    private function connect(): void
+    {
+        $dsn = "mysql:dbname={$this->name};host={$this->host};charset=utf8";
         try {
-            $pdo = new PDO($dsn, $user, $password, $drivers);
+            $this->pdo = new PDO($dsn, $this->user, $this->password, $this->drivers);
+//            echo "Connected successfully" . "\n";
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage() . "\n";
         }
-        return $pdo;
     }
+
+    public static function getInstance(array $configData) : Connection
+    {
+        if(!self::$instance) {
+            self::$instance = new Connection($configData);
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection() : PDO
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost(string $host): void
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $user
+     */
+    public function setUser(string $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @param array $drivers
+     */
+    public function setDrivers(array $drivers): void
+    {
+        $this->drivers = $drivers;
+    }
+
+
 
 }
